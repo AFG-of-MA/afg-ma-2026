e1f90a56324fbeb777e6d16e756c29e1caa36d9d
Stephen Cronin <638332+cronin4392@users.noreply.github.com>
2026-03-04 22:10:37 -0500

    Style footer

--- patterns/footers/footer-default.php
+++ patterns/footers/footer-default.php
@@ -8,57 +8,32 @@
  * @package wdsbt
  */
 
-// Determine whether to display site logo or site title.
-$wdsbt_site_info = has_custom_logo() ? '<!-- wp:site-logo {"width":200} /-->' : '<!-- wp:site-title /--><!-- wp:site-tagline /-->';
-
 // Generate the copyright information.
 $wdsbt_copyright_info = esc_html__( 'Copyright &copy; ', 'wdsbt' ) . esc_attr( gmdate( 'Y' ) );
 ?>
 
-<!-- wp:group {"tagName":"footer","metadata":{"name":"Footer"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"}}},"className":"alignfull","layout":{"type":"constrained"}} -->
-<footer class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70)">
+<!-- wp:group {"tagName":"footer","metadata":{"name":"Footer"}} -->
+<footer class="wp-block-group">
 
-	<!-- wp:columns {"style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"},"blockGap":{"top":"0"}}}} -->
-	<div class="wp-block-columns" style="padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70)">
+	<!-- wp:social-links {"iconColor":"black","iconColorValue":"#000","iconBackgroundColor":"white","iconBackgroundColorValue":"#fff","className":"is-style-default"} -->
+	<ul class="wp-block-social-links has-icon-color has-icon-background-color is-style-default">
 
-		<!-- wp:column {"width":"66.66%"} -->
-		<div class="wp-block-column" style="flex-basis:66.66%">
+		<!-- wp:social-link {"url":"https://www.facebook.com/webdevstudios","service":"facebook"} /-->
 
-			<?php echo wp_kses_post( $wdsbt_site_info ); ?>
+		<!-- wp:social-link {"url":"https://twitter.com/webdevstudios","service":"x"} /-->
 
-			<!-- wp:social-links {"iconColor":"black","iconColorValue":"#000","iconBackgroundColor":"white","iconBackgroundColorValue":"#fff","className":"is-style-default"} -->
-			<ul class="wp-block-social-links has-icon-color has-icon-background-color is-style-default">
+		<!-- wp:social-link {"url":"https://www.instagram.com/webdevstudios/","service":"instagram"} /-->
 
-				<!-- wp:social-link {"url":"https://www.facebook.com/webdevstudios","service":"facebook"} /-->
+		<!-- wp:social-link {"url":"https://www.linkedin.com/company/webdevstudios-llc-","service":"linkedin"} /-->
 
-				<!-- wp:social-link {"url":"https://twitter.com/webdevstudios","service":"x"} /-->
+		<!-- wp:social-link {"url":"https://www.youtube.com/channel/UCh3A6k9S5xKIh6nmKsTk0ag","service":"youtube"} /-->
 
-				<!-- wp:social-link {"url":"https://www.instagram.com/webdevstudios/","service":"instagram"} /-->
+	</ul>
+	<!-- /wp:social-links -->
 
-				<!-- wp:social-link {"url":"https://www.linkedin.com/company/webdevstudios-llc-","service":"linkedin"} /-->
-
-				<!-- wp:social-link {"url":"https://www.youtube.com/channel/UCh3A6k9S5xKIh6nmKsTk0ag","service":"youtube"} /-->
-
-			</ul>
-			<!-- /wp:social-links -->
-
-			<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"#f78766"}}}},"textColor":"white"} -->
-			<p class="has-white-color has-text-color has-link-color">WDS-BT stands for: <em>WebDevStudios Block Theme</em>. It can be found in the <a href="https://github.com/WebDevStudios/wds-bt">wds-bt github repo.</a>&nbsp; The theme is stood up on WDSLab: <a href="https://wdsbt.wdslab.com">https://wdsbt.wdslab.com</a>&nbsp;</p>
-			<!-- /wp:paragraph -->
-			<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"#f78766"}}}},"textColor":"white"} -->
-			<p class="has-white-color has-text-color has-link-color"><?php echo wp_kses_post( $wdsbt_copyright_info ); ?></p>
-			<!-- /wp:paragraph -->
-
-		</div>
-		<!-- /wp:column -->
-
-		<!-- wp:column {"width":"33.33%"} -->
-		<div class="wp-block-column" style="flex-basis:33.33%">
-		</div>
-		<!-- /wp:column -->
-
-	</div>
-	<!-- /wp:columns -->
+	<!-- wp:paragraph -->
+	<p class=""><?php echo wp_kses_post( $wdsbt_copyright_info ); ?></p>
+	<!-- /wp:paragraph -->
 
 </footer>
 <!-- /wp:group -->
